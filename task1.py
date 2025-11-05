84fb674546384cdf25c26ec0c0007f15bb8b0681
sindhuprya <sindhuprya24@gmail.com>
2025-11-05 21:37:16 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+# Task 1: Perform Basic Mathematical Operations
+
+# Take two numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform basic mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display the results
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+
---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2:
+
+# Take user's first name and last name as input
+first_name = input("Enter your first name: ").strip()
+last_name = input("Enter your last name: ").strip()
+
+# Concatenate to form the full name
+fullname = first_name + " " + last_name
+
+# Print a personalized greeting message
+print(f"Hello, {fullname}! Welcome to the Python programming world!")
